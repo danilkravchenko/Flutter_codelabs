e73922fff25bc0af90bdd96c1815153df0871549
dekaypvp <danikravchen@gmail.com>
2019-03-24 23:04:53 +0200

    chat with separate themes for ios and android

--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -1,14 +1,33 @@
 // This sample shows adding an action to an [AppBar] that opens a shopping cart.
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:friendlychat/widgets/ChatMessage.dart';
 
 void main() => runApp(MyApp());
+
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Friendlychat', home: ChatWidget());
+    return MaterialApp(
+        title: 'Friendlychat',
+        theme: defaultTargetPlatform == TargetPlatform.iOS
+            ? kIOSTheme
+            : kDefaultTheme,
+        home: ChatWidget());
   }
 }
 
@@ -17,41 +36,104 @@ class ChatWidget extends StatefulWidget {
   State createState() => ChatWidgetState();
 }
 
-class ChatWidgetState extends State<ChatWidget> {
+class ChatWidgetState extends State<ChatWidget> with TickerProviderStateMixin {
   final _textController = TextEditingController();
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  bool _isComposing = false;
 
-  Widget _buildTextComposer() => Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Row(
-        children: <Widget>[
-          Flexible(
-            child: TextField(
-              controller: _textController,
-              onSubmitted: _handleSubmitted,
-              decoration: InputDecoration.collapsed(hintText: "Send a message"),
-            ),
-          ),
-          Container(
-              margin: EdgeInsets.symmetric(horizontal: 4.0),
-              child: _buildSendButton())
-        ],
-      ));
+  Widget _buildTextComposer() => IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            children: <Widget>[
+              Flexible(
+                child: TextField(
+                  controller: _textController,
+                  onSubmitted: _handleSubmitted,
+                  onChanged: (String text) {
+                    setState(() {
+                      _isComposing = text.length > 0;
+                    });
+                  },
+                  decoration:
+                      InputDecoration.collapsed(hintText: "Send a message"),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.symmetric(horizontal: 4.0),
+                  child: _buildSendButton())
+            ],
+          )));
 
-  Widget _buildSendButton() => IconButton(
-      icon: Icon(Icons.send),
-      onPressed: () => _handleSubmitted(_textController.text));
+  Widget _buildSendButton() => Theme.of(context).platform == TargetPlatform.iOS
+      ? CupertinoButton(
+          child: Text("Send"),
+          onPressed: _isComposing
+              ? () => _handleSubmitted(_textController.text)
+              : null)
+      : IconButton(
+          icon: Icon(Icons.send),
+          onPressed: _isComposing
+              ? () => _handleSubmitted(_textController.text)
+              : null);
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+    final message = ChatMessage(
+      text: text,
+      animationController: AnimationController(
+          duration: Duration(milliseconds: 700), vsync: this),
+    );
+
+    setState(() {
+      _messages.insert(0, message);
+    });
+    message.animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _messages.forEach((el) => el.animationController.dispose());
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Friendlychat"),
-      ),
-      body: _buildTextComposer(),
-    );
+        appBar: AppBar(
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+          title: Text("Friendlychat"),
+        ),
+        body: Container(
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? BoxDecoration(
+                  border: Border(
+                    top: BorderSide(color: Colors.grey[200]),
+                  ),
+                )
+              : null,
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                child: ListView.builder(
+                  padding: EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (_, index) => _messages[index],
+                  itemCount: _messages.length,
+                ),
+              ),
+              Divider(height: 1.0),
+              Container(
+                decoration: BoxDecoration(color: Theme.of(context).cardColor),
+                child: _buildTextComposer(),
+              )
+            ],
+          ),
+        ));
   }
 }
