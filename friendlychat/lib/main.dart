719d5c33c17d7374738eab138d3220719da112d1
dekaypvp <danikravchen@gmail.com>
2019-03-24 12:55:31 +0200

    initial commit

--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -8,34 +8,50 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Code Sample for material.AppBar.actions',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyStatelessWidget(),
-    );
+    return MaterialApp(title: 'Friendlychat', home: ChatWidget());
   }
 }
 
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({Key key}) : super(key: key);
+class ChatWidget extends StatefulWidget {
+  @override
+  State createState() => ChatWidgetState();
+}
+
+class ChatWidgetState extends State<ChatWidget> {
+  final _textController = TextEditingController();
+
+  Widget _buildTextComposer() => Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: InputDecoration.collapsed(hintText: "Send a message"),
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: _buildSendButton())
+        ],
+      ));
+
+  Widget _buildSendButton() => IconButton(
+      icon: Icon(Icons.send),
+      onPressed: () => _handleSubmitted(_textController.text));
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello World'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            tooltip: 'Open shopping cart',
-            onPressed: () {
-              // ...
-            },
-          ),
-        ],
+        title: Text("Friendlychat"),
       ),
+      body: _buildTextComposer(),
     );
   }
 }
